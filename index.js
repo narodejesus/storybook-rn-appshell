ba5cb58281e05cc298883106bd04e4e9946cd7f6
Onar <onardejesus@hipagesgroup.com.au>
2023-01-19 10:29:14 +1100

    chore: storybook setup

--- index.js
+++ index.js
@@ -3,13 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
 
-import {StorybookUIRoot} from './storybook'
+import {StorybookUIRoot} from './storybook';
 import {name as appName} from './app.json';
 
-// AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(
-  appName,
-  () => StorybookUIRoot,
-);
+AppRegistry.registerComponent(appName, () => StorybookUIRoot);
