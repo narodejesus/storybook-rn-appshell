04b0a783d05454bbfb4539d6a190a104d62b7ba8
Onar <onardejesus@hipagesgroup.com.au>
2023-01-19 11:55:24 +1100

    chore: add storybook generator

--- storybook/index.tsx
+++ storybook/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
-import { LogBox, Platform } from 'react-native';
+import React from 'react';
+import {Platform} from 'react-native';
 
-import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
-import { withKnobs } from '@storybook/addon-knobs';
+import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
+import {withKnobs} from '@storybook/addon-knobs';
+import {loadStories} from './storyLoader';
 
 import './rn-addons';
 // LogBox.ignoreLogs(['Require cycle: node_modules/axios/lib/defaults.js']);
@@ -12,16 +13,16 @@ addDecorator(withKnobs);
 
 // import stories
 configure(() => {
- require('./stories');
+  loadStories();
 }, module);
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
 const StorybookUI = getStorybookUI({
-    host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
-    asyncStorage: null
+  host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
+  asyncStorage: null
 });
 
 export const StorybookUIRoot = () => {
-    return <StorybookUI />;
+  return <StorybookUI />;
 };
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,12 +4,44 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-    require('./stories/index');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/AnimatedTickIcon/AnimatedTickIcon.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/Banner/Banner.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/Chip/Chip.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/Dialog/Dialog.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/DialogBadge/DialogBadge.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPButton/HPButton.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPIcon/HPIcon.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPImageButton/HPImageButton.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPNameBadge/HPNameBadge.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPSnackbar/HPSnackbar.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPStatusBadge/HPStatusBadge.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPSwitch/HPSwitch.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPText/HPText.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPTextInput/HPTextInput.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/HPTextInputNumeric/HPTextInputNumeric.stories');
+  require('../node_modules/@hipagesgroup/toolbox-rn/src/NudgeCard/NudgeCard.stories');
 }
 
-const stories = ['./stories/index'];
+const stories = [
+  '../node_modules/@hipagesgroup/toolbox-rn/src/AnimatedTickIcon/AnimatedTickIcon.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/Banner/Banner.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/Chip/Chip.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/Dialog/Dialog.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/DialogBadge/DialogBadge.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPButton/HPButton.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPIcon/HPIcon.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPImageButton/HPImageButton.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPNameBadge/HPNameBadge.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPSnackbar/HPSnackbar.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPStatusBadge/HPStatusBadge.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPSwitch/HPSwitch.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPText/HPText.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPTextInput/HPTextInput.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/HPTextInputNumeric/HPTextInputNumeric.stories',
+  '../node_modules/@hipagesgroup/toolbox-rn/src/NudgeCard/NudgeCard.stories',
+];
 
 module.exports = {
-    loadStories,
-    stories
+  loadStories,
+  stories,
 };
